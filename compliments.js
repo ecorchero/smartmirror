c9d00c21d812a01aec2ae245de7a38a0fd3e9b60
Elena Corchero <elenacorchero@gmail.com>
2024-03-26 00:01:58 +0000

    Add files via upload

---
+++ compliments.js
@@ -0,0 +1,207 @@
+/* MagicMirror²
+ * Module: Compliments
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ */
+Module.register("compliments", {
+	// Module config defaults.
+	defaults: {
+		compliments: {
+			//title: "#JournalHouseDavos",
+			anytime: [
+				"Good to see you! Have you been working out?", 
+				"Welcome to Journal House. Go ahead, take a mirror selfie.",
+				"Welcome to Journal House, your home away from home during the World Economic Forum."],
+			morning: [
+				"Good morning! Love the outfit.",
+				"Good to see you! Ready to seize the day?",
+				"Good morning! Did you have breakfast? Grab some at the bar.",
+				"Good morning! Let's see your best Blue Steel.",
+				"Good morning! Enjoy today's interviews. Feel free to practice your speaker questions with me.",
+				"Good morning! Feel free to practice your selfie face."],
+			afternoon: [
+				"You look like you could use a snack. Grab one at the bar.",
+				"Welcome to Journal House. Grab a drink, warm up and enjoy today's interviews.",
+				"Good afternoon! How is your day going?",
+				"Good afternoon! Let's see your best Blue Steel.",
+				"Good afternoon! Enjoy today's interviews. Feel free to practice your speaker questions with me.",
+				"Good afternoon, welcome to Journal House. Looks like it's time to warm up, fuel up and catch an interview."],
+			evening: [
+				"Good evening, welcome to Journal House. Are you ready for a cocktail?",
+				"Good evening, welcome to Journal House. How was your day?",
+				"Good evening, welcome to Journal House. Let's have a drink, shall we?",
+				"Good evening! Turn off your step tracker, it's time to relax and unwind.",
+				"Good evening! A trip to the bar is about 30 steps, if you're counting.",
+				"Good evening! Would you say your day was straight up or on the rocks?"]
+			//"....-01-01": ["Happy new year!"]
+		},
+		updateInterval: 30000,
+		remoteFile: null,
+		fadeSpeed: 4000,
+		morningStartTime: 3,
+		morningEndTime: 12,
+		afternoonStartTime: 12,
+		afternoonEndTime: 17,
+		random: true
+	},
+	lastIndexUsed: -1,
+	// Set currentweather from module
+	currentWeatherType: "",
+
+	// Define required scripts.
+	getScripts: function () {
+		return ["moment.js"];
+	},
+
+	// Define start sequence.
+	start: async function () {
+		Log.info(`Starting module: ${this.name}`);
+
+		this.lastComplimentIndex = -1;
+
+		if (this.config.remoteFile !== null) {
+			const response = await this.loadComplimentFile();
+			this.config.compliments = JSON.parse(response);
+			this.updateDom();
+		}
+
+		// Schedule update timer.
+		setInterval(() => {
+			this.updateDom(this.config.fadeSpeed);
+		}, this.config.updateInterval);
+	},
+
+	/**
+	 * Generate a random index for a list of compliments.
+	 *
+	 * @param {string[]} compliments Array with compliments.
+	 * @returns {number} a random index of given array
+	 */
+	randomIndex: function (compliments) {
+		if (compliments.length === 1) {
+			return 0;
+		}
+
+		const generate = function () {
+			return Math.floor(Math.random() * compliments.length);
+		};
+
+		let complimentIndex = generate();
+
+		while (complimentIndex === this.lastComplimentIndex) {
+			complimentIndex = generate();
+		}
+
+		this.lastComplimentIndex = complimentIndex;
+
+		return complimentIndex;
+	},
+
+	/**
+	 * Retrieve an array of compliments for the time of the day.
+	 *
+	 * @returns {string[]} array with compliments for the time of the day.
+	 */
+	complimentArray: function () {
+		const hour = moment().hour();
+		const date = moment().format("YYYY-MM-DD");
+		let compliments = [];
+
+		// Add time of day compliments
+		if (hour >= this.config.morningStartTime && hour < this.config.morningEndTime && this.config.compliments.hasOwnProperty("morning")) {
+			compliments = [...this.config.compliments.morning];
+		} else if (hour >= this.config.afternoonStartTime && hour < this.config.afternoonEndTime && this.config.compliments.hasOwnProperty("afternoon")) {
+			compliments = [...this.config.compliments.afternoon];
+		} else if (this.config.compliments.hasOwnProperty("evening")) {
+			compliments = [...this.config.compliments.evening];
+		}
+
+		// Add compliments based on weather
+		if (this.currentWeatherType in this.config.compliments) {
+			Array.prototype.push.apply(compliments, this.config.compliments[this.currentWeatherType]);
+		}
+
+		// Add compliments for anytime
+		Array.prototype.push.apply(compliments, this.config.compliments.anytime);
+
+		// Add compliments for special days
+		for (let entry in this.config.compliments) {
+			if (new RegExp(entry).test(date)) {
+				Array.prototype.push.apply(compliments, this.config.compliments[entry]);
+			}
+		}
+
+		return compliments;
+	},
+
+	/**
+	 * Retrieve a file from the local filesystem
+	 *
+	 * @returns {Promise} Resolved when the file is loaded
+	 */
+	loadComplimentFile: async function () {
+		const isRemote = this.config.remoteFile.indexOf("http://") === 0 || this.config.remoteFile.indexOf("https://") === 0,
+			url = isRemote ? this.config.remoteFile : this.file(this.config.remoteFile);
+		const response = await fetch(url);
+		return await response.text();
+	},
+
+	/**
+	 * Retrieve a random compliment.
+	 *
+	 * @returns {string} a compliment
+	 */
+	getRandomCompliment: function () {
+		// get the current time of day compliments list
+		const compliments = this.complimentArray();
+		// variable for index to next message to display
+		let index;
+		// are we randomizing
+		if (this.config.random) {
+			// yes
+			index = this.randomIndex(compliments);
+		} else {
+			// no, sequential
+			// if doing sequential, don't fall off the end
+			index = this.lastIndexUsed >= compliments.length - 1 ? 0 : ++this.lastIndexUsed;
+		}
+
+		return compliments[index] || "";
+	},
+
+	// Override dom generator.
+	getDom: function () {
+		const wrapper = document.createElement("div");
+		wrapper.className = this.config.classes ? this.config.classes : "thin xlarge bright pre-line";
+		// get the compliment text
+		const complimentText = this.getRandomCompliment();
+		// split it into parts on newline text
+		const parts = complimentText.split("\n");
+		// create a span to hold the compliment
+		const compliment = document.createElement("span");
+		// process all the parts of the compliment text
+		for (const part of parts) {
+			if (part !== "") {
+				// create a text element for each part
+				compliment.appendChild(document.createTextNode(part));
+				// add a break
+				compliment.appendChild(document.createElement("BR"));
+			}
+		}
+		// only add compliment to wrapper if there is actual text in there
+		if (compliment.children.length > 0) {
+			// remove the last break
+			compliment.lastElementChild.remove();
+			wrapper.appendChild(compliment);
+		}
+		return wrapper;
+	},
+
+	// Override notification handler.
+	notificationReceived: function (notification, payload, sender) {
+		if (notification === "CURRENTWEATHER_TYPE") {
+			this.currentWeatherType = payload.type;
+		}
+	}
+});
---
+++ config.js
@@ -0,0 +1,127 @@
+/* MagicMirror² Config Sample
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information on how you can configure this file
+ * see https://docs.magicmirror.builders/configuration/introduction.html
+ * and https://docs.magicmirror.builders/modules/configuration.html
+ *
+ * You can use environment variables using a `config.js.template` file instead of `config.js`
+ * which will be converted to `config.js` while starting. For more information
+ * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
+ */
+let config = {
+	address: "localhost",	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/",			// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+					  		// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
+															// or add a specific IPv4 of 192.168.1.5 :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "en",
+	locale: "en-GB", // or "eng-US"
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric", // for celsius units are "metric", for Fahrenheit units are "imperial"
+
+	modules: [
+		/*{
+			module: "alert", //Activate this module if you want to review of any error alerts or need to update software
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},*/
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "weather",
+			position: "top_right",
+			config: {
+				weatherProvider: "openweathermap",
+				type: "current",
+				location: "Davos, CH",
+				locationID: "2661039", //or 2643743 for London, 5128581 for NYC, or 5364275 for Laguna Beach, 3028808 for Cannes, 2661039 for Davos 
+				//ID from https://openweathermap.org/find
+				//Find the city by name and country, then copy the code on the browser address
+				apiKey: "6da27524d67fa1b1eebd3b849449ac43"
+			}
+		},
+		/*{
+			module: "weather",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				weatherProvider: "openweathermap",
+				type: "forecast",
+				location: "New York",
+				locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				apiKey: "YOUR_OPENWEATHER_API_KEY"
+			}
+		},*/
+		{
+			module: "calendar",
+			header: "</br></br></br></br>UP NEXT",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "https://calendar.google.com/calendar/ical/4e406066c53614563fbf3d39a49ccd6f8a6e33816f0014106f43503713daa9d5%40group.calendar.google.com/public/basic.ics" //EMEA Mirror calendar, or for US Mirror calendar use "https://calendar.google.com/calendar/ical/7fab5c51af8c4a3b0ffe1b48cc8578c58323db20e48e451fc9cb7f2677a39310%40group.calendar.google.com/public/basic.ics"
+						
+					}
+				]
+			}
+		},
+		{
+			module: "compliments",
+			//header: "#JournalHouseDavos</br></br></br></br>",
+			//position: "lower_third",
+			position: "bottom_bar"
+		},
+
+		{
+			module: "newsfeed",
+			header: "WSJ NEWS</br></br>",
+			//position: "bottom_bar",
+			position: "lower_third",
+			config: {
+				feeds: [
+					/*{
+						title: "New York Times",
+						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					},*/
+					{
+						title: "WORLD NEWS",
+						url: "https://feeds.a.dj.com/rss/RSSWorldNews.xml" //or for Technology News "https://feeds.a.dj.com/rss/RSSWSJD.xml"
+					},
+					/*{
+						title: "WORLD NEWS",
+						url: "https://feeds.a.dj.com/rss/RSSWorldNews.xml"
+					}*/
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
